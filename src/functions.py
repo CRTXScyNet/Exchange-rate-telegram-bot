ed0eb76526d9e7329dd56718cdc41b05195057bb
CRTXScyNet <CRTXScyNet@gmail.com>
2024-11-08 16:24:28 +0500

    add to function.py write finction to write data to file and dounload func to download data from remote API

---
+++ src/functions.py
@@ -0,0 +1,35 @@
+import json
+import os.path
+
+from requests import request, get
+from win32ctypes.pywin32.pywintypes import datetime
+
+filepath = 'data.json'
+
+
+def write(my_dict: dict):
+    """Write dictionary as json to file"""
+    with open(filepath, 'w') as f:
+        json.dump(my_dict,f)
+
+def download_rates() -> dict:
+    """Download rates from remote API"""
+    url = "https://www.cbr-xml-daily.ru/daily_json.js"
+    my_dict = {}
+
+    response = get(url)
+
+    for rate in response.json()['Valute']:
+        my_dict[rate] = response.json()['Valute'][rate]['Value']
+
+def get_rate():
+    my_dict = {}
+    changed_dict = {}
+    if not os.path.exists(filepath):
+        my_dict = download_rates()
+        write(my_dict)
+        changed_dict =  my_dict.copy()
+
+    print(changed_dict)
+
+get_rate()
